54d0556e6f12643e5fa3dbf0d70df1622911f6d5
DANIEL ELIAS JIMENEZ ROJAS <dejimenez21@gmail.com>
2023-04-03 10:56:53 -0400

    Add Money value object with tests

---
+++ Domain.Test/ValueObjects/MoneyTest.cs
@@ -0,0 +1,108 @@
+﻿using Domain.Enums;
+using Domain.ValueObjects;
+using FluentAssertions;
+
+namespace Domain.Test.ValueObjects;
+
+public class MoneyTests
+{
+    [Fact]
+    public void Add_WithSameCurrency_ShouldReturnNewMoneyWithSumOfAmounts()
+    {
+        // Arrange
+        var money1 = new Money(10m, CurrencyCode.USD);
+        var money2 = new Money(20m, CurrencyCode.USD);
+
+        // Act
+        var result = money1.Add(money2);
+
+        // Assert
+        result.Amount.Should().Be(30m);
+        result.Currency.Should().Be(CurrencyCode.USD);
+    }
+
+    [Fact]
+    public void Add_WithDifferentCurrencies_ShouldThrowInvalidOperationException()
+    {
+        // Arrange
+        var money1 = new Money(10m, CurrencyCode.USD);
+        var money2 = new Money(20m, CurrencyCode.DOP);
+
+        // Act
+        void Action() => money1.Add(money2);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>((Action)Action);
+    }
+
+    [Fact]
+    public void Subtract_WithSameCurrency_ShouldReturnNewMoneyWithDifferenceOfAmounts()
+    {
+        // Arrange
+        var money1 = new Money(30m, CurrencyCode.USD);
+        var money2 = new Money(20m, CurrencyCode.USD);
+
+        // Act
+        var result = money1.Subtract(money2);
+
+        // Assert
+        result.Amount.Should().Be(10m);
+        result.Currency.Should().Be(CurrencyCode.USD);
+    }
+
+    [Fact]
+    public void Subtract_WithDifferentCurrencies_ShouldThrowInvalidOperationException()
+    {
+        // Arrange
+        var money1 = new Money(30m, CurrencyCode.USD);
+        var money2 = new Money(20m, CurrencyCode.DOP);
+
+        // Act
+        void Action() => money1.Subtract(money2);
+
+        // Assert
+        Assert.Throws<InvalidOperationException>((Action)Action);
+    }
+
+    [Fact]
+    public void Equals_WithEqualMoneyObjects_ShouldReturnTrue()
+    {
+        // Arrange
+        var money1 = new Money(10m, CurrencyCode.USD);
+        var money2 = new Money(10m, CurrencyCode.USD);
+
+        // Act
+        var result = money1.Equals(money2);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Equals_WithDifferentAmounts_ShouldReturnFalse()
+    {
+        // Arrange
+        var money1 = new Money(10m, CurrencyCode.USD);
+        var money2 = new Money(20m, CurrencyCode.USD);
+
+        // Act
+        var result = money1.Equals(money2);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Equals_WithDifferentCurrencies_ShouldReturnFalse()
+    {
+        // Arrange
+        var money1 = new Money(10m, CurrencyCode.USD);
+        var money2 = new Money(10m, CurrencyCode.DOP);
+
+        // Act
+        var result = money1.Equals(money2);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
---
+++ Domain/ValueObjects/Money.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Enums;
+
+namespace Domain.ValueObjects
+{
+    public class Money : IEquatable<Money>
+    {
+        public decimal Amount { get; }
+        public CurrencyCode Currency { get; }
+
+        public Money(decimal amount, CurrencyCode currency)
+        {
+            Amount = amount;
+            Currency = currency;
+        }
+
+        public Money Add(Money other)
+        {
+            if (Currency != other.Currency)
+            {
+                throw new InvalidOperationException("Cannot add amounts with different currencies");
+            }
+
+            return new Money(Amount + other.Amount, Currency);
+        }
+
+        public Money Subtract(Money other)
+        {
+            if (Currency != other.Currency)
+            {
+                throw new InvalidOperationException("Cannot subtract amounts with different currencies");
+            }
+
+            return new Money(Amount - other.Amount, Currency);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Money);
+        }
+
+        public bool Equals(Money other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Amount == other.Amount && Currency == other.Currency;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
+                hash = (hash * 23) + Amount.GetHashCode();
+                hash = (hash * 23) + Currency.GetHashCode();
+                return hash;
+            }
+        }
+
+        public static bool operator ==(Money left, Money right)
+        {
+            if (left is null)
+            {
+                return right is null;
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Money left, Money right)
+        {
+            return !(left == right);
+        }
+    }
+}
