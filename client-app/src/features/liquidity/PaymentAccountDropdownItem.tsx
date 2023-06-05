fd01720080e0967b04d63a817aec5755db35a93a
dejimenez21 <dejimenez21@gmail.com>
2023-06-05 09:38:42 -0400

    Format and remove unused using

--- client-app/src/features/liquidity/PaymentAccountDropdownItem.tsx
+++ client-app/src/features/liquidity/PaymentAccountDropdownItem.tsx
@@ -1,7 +1,6 @@
-import { Grid, Header, Image } from "semantic-ui-react";
+import { Grid, Image } from "semantic-ui-react";
 import utils from "../../app/utils/assetsUtils";
 import PaymentAccount from "../../app/models/expenses/paymentAccount";
-import { paymentMethods } from "../../app/models/expenses/paymentMethod";
 
 interface Props {
   account: PaymentAccount;
@@ -23,14 +22,16 @@ const PaymentAccountDropdownItem = ({ account }: Props) => {
           {account.name}
         </Grid.Column>
         {account.network ? (
-        <Grid.Column width={1} textAlign="right" verticalAlign="middle">
-          <Image
-            src={utils.getPaymentNetworkImage(account.network!)}
-            size="mini"
-            floated="right"
-          />
-        </Grid.Column>
-        ) : ""} 
+          <Grid.Column width={1} textAlign="right" verticalAlign="middle">
+            <Image
+              src={utils.getPaymentNetworkImage(account.network!)}
+              size="mini"
+              floated="right"
+            />
+          </Grid.Column>
+        ) : (
+          ""
+        )}
       </Grid.Row>
     </Grid>
   );
