6b21ebfa7d17b0922862b4d354a809f79f70bdc5
dejimenez21 <dejimenez21@gmail.com>
2023-06-29 15:16:23 -0400

    Switch database provider for development environment from sqlite to postgresql
    - Added a Local environment that uses Sqlite in case is needed

--- Api/Extensions/DbContextExtension.cs
+++ Api/Extensions/DbContextExtension.cs
@@ -10,12 +10,12 @@ public static class DbContextExtension
     public static void RegisterAllDbContexts(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment env)
     {
         // Register DbContext as Singleton?
-        if (env.IsDevelopment())
+        if (env.IsEnvironment("Local"))
         {
             services.AddDbContext<ExpensesDbContext>(opt => opt.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<TransactionsDbContext>(opt => opt.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
         }
-        else if(env.IsStaging())
+        else
         {
             services.AddDbContext<ExpensesDbContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<TransactionsDbContext>(opt => opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
